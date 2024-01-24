de23df7836a752dc32be9fecd8121e2b99340e2e
Aman Singh <amanadhikariaps@gmail.com>
2024-01-24 22:20:30 +0530

    commiting whole project

---
+++ project.pkr.hcl
@@ -0,0 +1,58 @@
+# If you have your default VPC available then use it. 
+
+# packer puglin for AWS 
+# https://www.packer.io/plugins/builders/amazon 
+packer {
+  required_plugins {
+    amazon = {
+      version = ">= 0.0.2"
+      source  = "github.com/hashicorp/amazon"
+    }
+  }
+}
+
+# which ami to use as the base and where to save it
+source "amazon-ebs" "amazon-linux" {
+  region          = "ap-south-1"
+  ami_name        = "aman-ami-version-1-{{timestamp}}"
+  instance_type   = "t2.micro"
+  source_ami      = "ami-0d3f444bc76de0a79"
+  ssh_username    = "ec2-user"
+  #ami_users       = ["AWS Account ID"]
+  ami_regions     = [
+                      "ap-south-1"
+                    ]
+}
+
+# what to install, configure and file to copy/execute
+build {
+  name = "hq-packer"
+  sources = [
+    "source.amazon-ebs.amazon-linux"
+  ]
+
+  provisioner "file" {
+  source = "provisioner.sh"
+  destination = "/tmp/provisioner.sh"
+}
+
+  provisioner "shell" {
+    inline = ["chmod a+x /tmp/provisioner.sh"]
+  }
+  
+  provisioner "shell" {
+    inline = [ "ls -la /tmp"]
+  }
+  
+    provisioner "shell" {
+    inline = [ "pwd"]
+  }
+  
+  provisioner "shell" {
+    inline = [ "cat /tmp/provisioner.sh"]
+  }
+
+  provisioner "shell" {
+    inline = ["/bin/bash -x /tmp/provisioner.sh"]
+  }
+}
---
+++ provisioner.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# update package
+sudo yum -y update
+
+# install git
+sudo yum install git -y
+
+# install SSM
+sudo yum install https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm -y
+#sudo systemctl start amazon-ssm-agent
+
+# install cloudwatch agent
+sudo yum install https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm -y
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a start
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
+
+# install AWS inspector
+curl -O https://inspector-agent.amazonaws.com/linux/latest/install
+sudo bash install
+
+#insall docker 
+sudo yum install docker -y
+sudo systemctl start docker
+
+#install mongod
+sudo apt-get install gnupg curl
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl daemon-reload
+sudo systemctl start mongod
