4bea1cc4647a6d2322977d0dbbd2f940d9894ce6
Aman Singh <102499131+AmanSingh881@users.noreply.github.com>
2024-02-02 22:11:31 +0530

    Update provisioner.sh

--- provisioner.sh
+++ provisioner.sh
@@ -22,14 +22,3 @@ sudo bash install
 #insall docker 
 sudo yum install docker -y
 sudo systemctl start docker
-
-#install mongod
-sudo apt-get install gnupg curl
-curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-   --dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo systemctl daemon-reload
-sudo systemctl start mongod
